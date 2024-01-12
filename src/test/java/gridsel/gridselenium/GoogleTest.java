135afefd0d04e9387dcfab9ed64dfad4e7e59a52
juhi <juhilohani1910@gmail.com>
2024-01-12 19:25:42 +0530

    first commit

---
+++ src/test/java/gridsel/gridselenium/GoogleTest.java
@@ -0,0 +1,22 @@
+package gridsel.gridselenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	@Test
+	public void HomePageCheck() throws MalformedURLException {
+		DesiredCapabilities caps=new DesiredCapabilities();
+		caps.setBrowserName("chrome");
+		WebDriver driver=new RemoteWebDriver(new URL("http://192.168.1.12:4444"),caps);
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("juhi lohani");
+		driver.close();
+	}
+}
---
+++ src/test/java/gridsel/gridselenium/RSATest.java
@@ -0,0 +1,20 @@
+package gridsel.gridselenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class RSATest {
+	@Test
+	public void HomePageCheck() throws MalformedURLException {
+		DesiredCapabilities caps=new DesiredCapabilities();
+		caps.setBrowserName("firefox");
+		WebDriver driver=new RemoteWebDriver(new URL("http://192.168.1.12:4444"),caps);
+		driver.get("http://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+}
