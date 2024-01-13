7174d1e335aafd3bac203db116c3a1549cb9d150
juhi <juhilohani1910@gmail.com>
2024-01-13 18:06:07 +0530

    American development team

--- src/test/java/gridsel/gridselenium/RSATest.java
+++ src/test/java/gridsel/gridselenium/RSATest.java
@@ -11,10 +11,9 @@ public class RSATest {
 	@Test
 	public void HomePageCheck() throws MalformedURLException {
 		DesiredCapabilities caps=new DesiredCapabilities();
-		caps.setBrowserName("firefox");
+		caps.setBrowserName("chrome");
 		WebDriver driver=new RemoteWebDriver(new URL("http://192.168.1.12:4444"),caps);
-		driver.get("http://rahulshettyacademy.com");
+		driver.get("http://google.com");
 		System.out.println(driver.getTitle());
-		driver.close();
 	}
 }
